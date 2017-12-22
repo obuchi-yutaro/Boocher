44ac9a9a6f287ae5febf00d0eaaed3176dcb3f8a
github user name <adetokunbopaker@yahoo.co.jp>
2017-12-22 18:00:07 +0900

    Make review new.html

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,17 @@ class ReviewsController < ApplicationController
   end
 
   def new
-
+    @user = User.find(params[:user_id])
+    @review = Review.new
   end
 
   def create
-
+    Review.create(create_params)
   end
 
+
+  private
+  def create_params
+    params.require(:review).permit(:rate, :text).merge(to_user_id: params[:user_id], from_user_id: current_user.id)
+  end
 end
